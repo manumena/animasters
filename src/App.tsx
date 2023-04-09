44e9a0f439f1719e49106941951b530e08c5925b
Manuel Mena <manuelmena1993@gmail.com>
2023-04-09 00:38:08 -0300

    feat: add song

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './pocha.png';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <audio 
+          controls={true}
+          loop={true}
+          src="https://firebasestorage.googleapis.com/v0/b/guess-the-anime-a6470.appspot.com/o/kick_back.mp3?alt=media&token=2b9963d9-a151-429e-aad7-217f860ef5e1"
+        />
         <p>Pochita appreciation page</p>
         <a
           className="App-link"
