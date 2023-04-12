3b604372eefdd4260c3ceb8d217795c07d7e2dbe
Manuel Mena <manuelmena1993@gmail.com>
2023-04-12 19:27:56 -0300

    feat: make names form work

--- src/components/PlayersNamesForm.tsx
+++ src/components/PlayersNamesForm.tsx
@@ -1,18 +1,44 @@
+import { ChangeEvent, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { HiOutlinePlus } from 'react-icons/hi'
 
 export default function PlayersNamesForm() {
+
+  const [ playerNames, setNames] = useState<string[]>([''])
+  const [ disabled, setDisabled ] = useState<boolean>(true)
+
+  function handleNameChange(e: ChangeEvent<HTMLInputElement>, id: number) {
+    const updatednames = playerNames
+    updatednames[id] = e.currentTarget.value
+    setNames(updatednames)
+    setDisabled(!everyNameIsFilled())
+  }
+
+  function handleClickPlus() {
+    const updatedNames = [...playerNames, '']
+    setNames(updatedNames)
+    setDisabled(true)
+  }
+
+  function handleClickStart() {
+    console.log('click')
+  }
+
+  
+  function everyNameIsFilled() {
+    return playerNames.every((name) => name.length > 0)
+  }
+  
   return (
     <>
       <div>
         <Form className='players-form'>
-          <Form.Control type="text" placeholder="Player name..." />
-          <Form.Control type="text" placeholder="Player name..." />
-          <Button variant="outline-light" className='plus-button'><HiOutlinePlus/></Button>
+          { playerNames.map((name, id) => <Form.Control key={id} type="text" placeholder="Enter player name..." onChange={(e: ChangeEvent<HTMLInputElement>) => handleNameChange(e, id)} />)}
+          <Button variant="outline-light" className='plus-button' onClick={handleClickPlus}><HiOutlinePlus/></Button>
         </Form>
       </div>
-      <Button variant="outline-light">Start</Button>
+      <Button variant="outline-light" disabled={disabled} onClick={handleClickStart}>Start</Button>
     </>
   )
 }
