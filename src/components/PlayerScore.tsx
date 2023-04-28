22ba05e563ee25e614cfc7c624101dbbdfe4a0a9
Manuel Mena <manuelmena1993@gmail.com>
2023-04-28 04:44:29 -0300

    feat: add player scores

--- src/components/PlayerScore.tsx
+++ src/components/PlayerScore.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
+import { Button } from "react-bootstrap"
+import { HiOutlineMinus, HiOutlinePlus } from "react-icons/hi"
+
 interface PlayerScoreProps {
   name: string
 }
 
 export default function PlayerScore({ name }: PlayerScoreProps) {
+
+  const [ score, setScore ] = useState<number>(0)
+
+  function handleClickPlus() {
+    setScore(score + 1)
+  }
+
+  function handleClickMinus() {
+    setScore(score - 1)
+  }
+
   return (
-    <div>
-      {name}
+    <div className="player-score-container">
+      <div className="player-name">{name}</div>
+      <div className="player-score">{score}</div>
+      <Button variant="outline-light" onClick={handleClickPlus}><HiOutlinePlus/></Button>
+      <Button variant="outline-light" onClick={handleClickMinus}><HiOutlineMinus/></Button>
     </div>
   )
 }
