3305438a712d47928e3d984a267b5c5d125cdb4c
Manuel Mena <manuelmena1993@gmail.com>
2023-04-28 03:20:12 -0300

    refactor: control game flow from parent

--- src/components/PlayersNamesForm.tsx
+++ src/components/PlayersNamesForm.tsx
@@ -3,7 +3,11 @@ import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { HiOutlinePlus } from 'react-icons/hi'
 
-export default function PlayersNamesForm() {
+interface PlayersNamesFormProps {
+  startCallback: (playerNames: string[]) => void
+}
+
+export default function PlayersNamesForm({ startCallback }: PlayersNamesFormProps) {
 
   const [ playerNames, setNames] = useState<string[]>([''])
   const [ disabled, setDisabled ] = useState<boolean>(true)
@@ -21,24 +25,21 @@ export default function PlayersNamesForm() {
     setDisabled(true)
   }
 
-  function handleClickStart() {
-    console.log('click')
-  }
-
-  
   function everyNameIsFilled() {
     return playerNames.every((name) => name.length > 0)
   }
   
   return (
     <>
+      <h1>AniMasters</h1>
+      <p>Immerse yourself in the captivating world of anime and test your knowledge by trying to guess the anime series from its opening theme</p>
       <div>
         <Form className='players-form'>
           { playerNames.map((name, id) => <Form.Control key={id} type="text" placeholder="Enter player name..." onChange={(e: ChangeEvent<HTMLInputElement>) => handleNameChange(e, id)} />)}
           <Button variant="outline-light" className='plus-button' onClick={handleClickPlus}><HiOutlinePlus/></Button>
         </Form>
       </div>
-      <Button variant="outline-light" disabled={disabled} onClick={handleClickStart}>Start</Button>
+      <Button variant="outline-light" disabled={disabled} className='start-button' onClick={() => {startCallback(playerNames)}}>Start</Button>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import PlayersNamesForm from "../components/PlayersNamesForm";
 
+type Song = {
+  id: number
+  name: string
+  anime: string
+  path: string
+}
+
+enum State { HOME, PLAYING, RESULTS }
+
 export default function Home() {
+
+  let players: string[]
+  let songs: Song[]
+  const [ state, setState ] = useState<State>(State.HOME)
+
+  function handleClickStart(playerNames: string[]) {
+    //Set players names
+    players = playerNames
+
+    // Request a match to match-generator
+    fetch('https://match-generator.manuelmena19938676.workers.dev/generate-match')
+      .then(response => response.json())
+      .then(data => {
+        songs = data.match
+        setState(State.PLAYING)
+      })
+  }
+
   return (
     <div className="home-container">
-      <h1>Otaku challenge</h1>
-      <p>Immerse yourself in the captivating world of anime and test your knowledge by trying to guess the anime series from its opening theme</p>
-      <PlayersNamesForm/> 
+      { state === State.HOME ? <PlayersNamesForm startCallback={handleClickStart} /> : <></> }
     </div>
   );
 }
