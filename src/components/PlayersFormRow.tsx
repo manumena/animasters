17aa7c4d7d6ddd2969cf1904bc68b652e0d6fef6
Manuel Mena <manuelmena1993@gmail.com>
2023-05-01 00:25:29 -0300

    feat: focus new input when added player

--- src/components/PlayersFormRow.tsx
+++ src/components/PlayersFormRow.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { HiOutlinePlus } from "react-icons/hi";
 import { RxCross1 } from "react-icons/rx";
@@ -13,13 +13,26 @@ interface PlayersFormRowProps {
 }
 
 export default function PlayersFormRow({name, totalPlayers, id, onChangeCallback, onClickPlusCallback, onClickMinusCallback}: PlayersFormRowProps) {
+
+  const ref = useRef(getNull());
+
+  useEffect(() => {
+    if (ref.current)
+      ref.current.focus()
+  }, [])
+  
   return (
     <div>
-      <Form.Control value={name} type="text" placeholder="Enter player name..." onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeCallback(e, id)} />
+      <Form.Control ref={ref} value={name} type="text" placeholder="Enter player name..." onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeCallback(e, id)} />
       { id === totalPlayers - 1 ? 
         <Button variant="outline-light" className='plus-button' onClick={onClickPlusCallback}><HiOutlinePlus/></Button> :
         <Button variant="outline-light" className='minus-button' onClick={() => onClickMinusCallback(id)}><RxCross1/></Button>
       }
     </div>
   )
+}
+
+// The compiler infers that useRef(null) can never be anything other than null https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-never-type
+function getNull(): any | null {
+  return null
 }
