96be6996d7442342deb0c3a89f3b25da5cb724b6
Manuel Mena <manuelmena1993@gmail.com>
2025-07-27 14:20:02 -0300

    refactor: remove unused import

--- src/components/PlayersFormRow.tsx
+++ src/components/PlayersFormRow.tsx
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+// import styled from "@emotion/styled";
 import { ChangeEvent, useEffect, useRef } from "react";
 import { Form } from "react-bootstrap";
 import { HiOutlinePlus } from "react-icons/hi";
